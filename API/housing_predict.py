e38797ffd6af97efb98dbb633dd18069af50b3e4
vietlinhp <v.l.pham@student.tue.nl>
2024-10-26 17:13:40 +0200

    Update housing_predict.py

--- API/housing_predict.py
+++ API/housing_predict.py
@@ -54,10 +54,10 @@ class HousingPredictor:
         y_pred = self.model.predict(df)
         logging.info(y_pred[0])
         #print(f"Prediction Result: {y_pred[0]}")
-        #status = (y_pred[0] > 0.5)
+        status = (y_pred[0] > 0.5)
         #logging.info(f"Prediction Status: {status}")
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': str(y_pred[0])}), 200
+        return jsonify({'result': str(status)}), 200
 if __name__ == "__main__":
     
     logging.basicConfig(level=logging.DEBUG)

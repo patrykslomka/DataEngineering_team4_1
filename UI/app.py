52dc5453865c97d550e1ab5564b990eaf7f218fe
whyyyyy99 <165992947+whyyyyy99@users.noreply.github.com>
2024-10-28 18:23:40 +0100

    Update app.py

--- UI/app.py
+++ UI/app.py
@@ -22,11 +22,10 @@ def housing_predict():
                 "Median_Income": float(request.form.get("Median_Income")),  # getting input with name = ntp in HTML form
                 "Median_Age": int(request.form.get("Median_Age")),  # getting input with name = pgc in HTML form
                 "Tot_Rooms": int(request.form.get("Tot_Rooms")),
-                "Tot_Bedrooms": float(request.form.get("Tot_Bedrooms")),
-                "Population": float(request.form.get("Population")),
-                "Households": float(request.form.get("Households")),
-                "Latitude": int(request.form.get("Latitude")),
+                "Tot_Bedrooms": int(request.form.get("Tot_Bedrooms")),
                 "Population": int(request.form.get("Population")),
+                "Households": int(request.form.get("Households")),
+                "Latitude": float(request.form.get("Latitude")),
                 "Longitude": float(request.form.get("Longitude")),
                 "Distance_to_coast": float(request.form.get("Distance_to_coast")),
                 "Distance_to_LA": float(request.form.get("Distance_to_LA")),
@@ -38,7 +37,6 @@ def housing_predict():
         ]
 
         logging.debug("Prediction input : %s", prediction_input)
-        prediction_input_json = json.dumps(prediction_input)
 
         # use requests library to execute the prediction service API by sending an HTTP POST request
         # use an environment variable to find the value of the diabetes prediction API
@@ -46,12 +44,8 @@ def housing_predict():
         # json.loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary.
         predictor_api_url = os.environ['PREDICTOR_API']
         res = requests.post(predictor_api_url, json=json.loads(json.dumps(prediction_input)))
-        logging.info(res.json())
-        print(res.json())
+
         prediction_value = res.json()['result']
-        #res = requests.post(predictor_api_url, json=json.loads(json.dumps(prediction_input)))
-        #logging.info("Status Code: %s", res.status_code)
-        #logging.info("Response Content: %s", res.text)
         logging.info("Prediction Output : %s", prediction_value)
         return render_template("response_page.html",
                                prediction_variable=prediction_value)
@@ -65,4 +59,4 @@ def housing_predict():
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8888)), host='0.0.0.0', debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)

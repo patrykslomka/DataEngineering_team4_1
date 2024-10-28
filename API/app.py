2e91e3156d2f61555995588bd891bfe33736ab67
vietlinhp <v.l.pham@student.tue.nl>
2024-10-28 12:09:20 +0100

    Update app.py

--- API/app.py
+++ API/app.py
@@ -8,17 +8,16 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/housing_predict/', methods=['PUT'])  # trigger updating the model
-def refresh_model():
-    return dp.download_model()
-
-
-@app.route('/housing_predict/', methods=['POST'])  # path of the endpoint. Except only HTTP POST request
+@app.route('/housing_predict/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
 def predict_str():
     # the prediction input data in the message body as a JSON payload
-    prediction_inout = request.get_json()
-    return dp.predict_single_record(prediction_inout)
+    prediction_input = request.get_json()
+    print(prediction_input)
+    return dp.predict_single_record(prediction_input)
 
 
 dp = HousingPredictor()
-app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+# The code within this conditional block will only run the python file is executed as a
+# script. See https://realpython.com/if-name-main-python/
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
